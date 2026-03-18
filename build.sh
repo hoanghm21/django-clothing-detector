aa47ec9accb8d968173d845af19f95ef30b15e02
hoanghm21 <amazinglife2211@gmail.com>
2026-03-18 13:50:43 +0700

    Use requirements-prod.txt to exclude heavy ML libs in production

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 # Thoát ngay nếu có lỗi
 set -o errexit
 
-# Cài đặt thư viện
-pip install -r requirements.txt
+# Cài đặt thư viện (production dùng file nhẹ hơn)
+if [ -f requirements-prod.txt ]; then
+    pip install -r requirements-prod.txt
+else
+    pip install -r requirements.txt
+fi
 
 # Gom các file static (CSS, JS, Images) để Render phục vụ
 python manage.py collectstatic --no-input
